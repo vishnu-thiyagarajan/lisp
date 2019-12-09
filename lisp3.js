f078b96afa1e737d6e97174056d740b78cdaa0b2
vishnu-thiyagarajan <33596700+vishnu-thiyagarajan@users.noreply.github.com>
2019-12-09 16:34:20 +0530

    recursive logic implemented

--- lisp3.js
+++ lisp3.js
@@ -46,24 +46,21 @@ function normalForm(operator, input, dict = envDict) {
     arr.push(result);
     input = rest;
   }
-  return [envDict[operator](arr), rest.slice(1)];
+  return [envDict[operator](arr), input];
 }
 
 function lambdaParser(input, dict = envDict) {
-  let obj = {};
   let match = /^ *\((.+?)\) +(.+)/.exec(input);
   if (!match) return null;
-  let rest,
+  let rest, result,
     vars = match[1]
   let value = function (val) {
+    let obj = {};
     for (key in vars) {
       atomVal = atomicValue(val[key]);
       obj[vars[key]] = atomVal ? atomVal[0] : null;
     }
-    do {
-      rest = match[2];
-      [result, rest] = evaluator(rest, obj);
-    } while (rest);
+      [result, rest] = evaluator(match[2], obj);
     return result;
   }
   return [value,rest];
@@ -83,10 +80,10 @@ function quoteParser(input) {
 }
 
 function ifParser(input, dict = envDict) {
-  var [result, rest] = evaluator(input,dict);
-  var [pass, rest] = evaluator(rest,dict);
-  var [fail, rest] = evaluator(rest,dict);
-  return result ? [pass, rest] : [fail, rest];
+  let match = /^ *(\(.+?\)) +(.+?) +(.+)/.exec(input)
+  if (!match) return null
+  var [result, rest] = evaluator(match[1],dict);
+  return result ? evaluator(match[2],dict) : evaluator(match[3],dict)
 }
 
 function defParser(input, dict = envDict) {
@@ -108,7 +105,7 @@ function beginParser(input, dict) {
 function atomicValue(input, dict = envDict) {
   let float = /^ *([0-9]+\.[0-9]+)? *(.*)/.exec(input);
   if (float && float[1]) return [parseFloat(float[1]), float[2]];
-  let number = /^ *([0-9]+)? *(.*)/.exec(input);
+  let number = /^ *([+-]?[0-9]+)? *(.*)/.exec(input);
   if (number && number[1]) return [parseInt(number[1]), number[2]];
   let str = /^ *(\"([0-9a-zA-Z]+)\")? *(.*)/.exec(input);
   if (str && str[1]) return [str[2], str[3]];
@@ -120,19 +117,17 @@ function atomicValue(input, dict = envDict) {
 
 function evaluator(input, dict = envDict) {
   let result,innerRest;let outterRest = input;
-  do {
-    let isAtomic = atomicValue(outterRest, dict);
-    if (isAtomic) return isAtomic;
-    let grps = new RegExp("^ *\\\( *(.+?) +(.*)", "ims").exec(outterRest);
-    if (grps) [innerRest,outterRest] = quoteParser(grps[2])
-    if (grps && grps[1] == "quote") return [innerRest,outterRest]
-    if (grps && grps[1] in ref) {
-      if (innerRest) [result,innerRest] = ref[grps[1]](innerRest, dict)
-    }
-    if (grps && grps[1] in envDict) {
-      if (innerRest) [result,innerRest] = normalForm(grps[1], innerRest, dict)
-    }
-  } while (outterRest)
+  let isAtomic = atomicValue(outterRest, dict);
+  if (isAtomic) return isAtomic;
+  let grps = new RegExp("^ *\\\( *(.+?) +(.*)", "ims").exec(outterRest);
+  if (grps) [innerRest,outterRest] = quoteParser(grps[2])
+  if (grps && grps[1] == "quote") return [innerRest,outterRest]
+  if (grps && grps[1] in ref) {
+    if (innerRest) [result,innerRest] = ref[grps[1]](innerRest, dict)
+  }
+  if (grps && grps[1] in envDict) {
+    if (innerRest) [result,innerRest] = normalForm(grps[1], innerRest, dict)
+  }
   return [result, outterRest]
 }
 // console.log(evaluator('(begin (+ 20 10) (define s "made"))'))
@@ -148,9 +143,24 @@ function evaluator(input, dict = envDict) {
 // console.log(evaluator('(max 4 3 2)'))
 // console.log(evaluator('(begin (define r (+ 20 10)) (define s 10))'))
 // console.log(evaluator('(begin (define r 20) (+ r 2))'))
-// console.log(evaluator("(define circle (lambda (r) (* 3.14 (* r r)))) (circle 10)"))
-// console.log(evaluator('(define fact (lambda (n) (if (<= n 1) 1 (* n (fact (- n 1)))))) (fact 4)'))
-// console.log(evaluator('(if (<= n 1) 1 (* n 5))',{n:4}))
-console.log(String.fromCharCode(16))
-console.log(String.fromCodePoint(1234))
-console.log(parseInt("0x0010"))
+// console.log(evaluator("(define circle (lambda (r) (* 3.14 (* r r))))"))
+// console.log(evaluator('(circle 10)'))
+// console.log(evaluator('(define fact (lambda (n) (if (<= n 1) 1 (* n (fact (- n 1))))))'))
+// console.log(evaluator('(fact 4)'))
+// console.log(evaluator('(define fib (lambda (n) (if (< n 2) 1 (+ (fib (- n 1)) (fib (- n 2))))))'))
+// console.log(evaluator('(fib 5)'))
+// console.log(evaluator('(circle (fact 10))'))
+console.log(evaluator('(define twice (lambda (x) (* 2 x)))'))
+// console.log(evaluator('(twice (twice (twice 5)))'))
+console.log(evaluator('(define repeat (lambda (f) (lambda (x) (f (f x)))))'))
+console.log(evaluator('((repeat twice) 10)'))
+// console.log(evaluator('(test 10)'))
+// let x = envDict.repeat(envDict.twice)
+// console.log(x(2))
+// console.log(evaluator('(define val (repeat twice))'))
+// console.log(evaluator('(val 10)'))
+// console.log(evaluator('(define z (lambda (x) (lambda (x) (+ x 1))))'))
+// console.log(evaluator('((z 10) 12)'))
+
+
+
